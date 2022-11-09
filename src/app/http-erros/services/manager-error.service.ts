69fa3fd8430e5d624f5ad5d1aa5493ebdf751b90
Dario_herrera <d.herrerag@uniandes.edu.co>
2022-11-08 20:56:16 -0500

    bug fix repeated conditional structure fixed for unittest

--- src/app/http-erros/services/manager-error.service.ts
+++ src/app/http-erros/services/manager-error.service.ts
@@ -10,7 +10,7 @@ export class ManagerErrorService {
   constructor(private router: Router) { }
 
   setStatusCode(status: number): void  {
-     if (status === 0 || status === 500 || status === 404 ) {
+     if (status === 0 || status === 500 || status === 404 || status === 504 ) {
        this.router.navigate([Routes.ERROR_INTERNET]);
      }
     }
